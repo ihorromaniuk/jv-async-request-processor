ef43814c1ffd86e9e578c608b62bc5782ebba361
Ihor Romaniuk <ihor.rome@gmail.com>
2025-08-28 11:06:12 +0300

    add solution

--- src/main/java/mate/academy/AsyncRequestProcessor.java
+++ src/main/java/mate/academy/AsyncRequestProcessor.java
@@ -11,6 +11,13 @@ public class AsyncRequestProcessor {
     }
 
     public CompletableFuture<UserData> processRequest(String userId) {
-        return null;
+        return CompletableFuture.supplyAsync(() -> {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return new UserData(userId, "Details for " + userId);
+        }, executor);
     }
 }
